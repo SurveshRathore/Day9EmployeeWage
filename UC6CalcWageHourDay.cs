861b465d1ac137860e51c7e9b51b017e69b7f16d
SurveshRathore <educatedevilboy@gmail.com>
2022-10-06 19:25:16 +0530

    Use Case 5 to calculating the wages till a condiittion of total working hours or days is reached for a month

---
+++ UC6CalcWageHourDay.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4PPEmpWageComputation
+{
+    //UC6CalcWageHourDay class 
+    internal class UC6CalcWageHourDay
+    {
+        //calculating the wages till a condition of total working hours or days is reached for a month
+        public void CalcWageForWorkingHourDay ()
+        {
+            //variable to store the employee hourly rate
+            int EMP_HOUR_RATE = 80;
+            //variable to set the hours per month
+            int EMP_MONTH_HOURS = 100;
+            //variable to set the days per month
+            int EMP_MONTH_DAYS = 20;
+            //variable to store the hour
+            int hour = 0;
+            //variable to count the days
+            int days = 0;
+            //variable to store the employee monthly wage
+            int MonthlyWage = 0;
+
+            //Create an object of the Random() class
+            Random random = new Random();            
+
+            while (hour <= EMP_MONTH_HOURS && days < EMP_MONTH_DAYS)
+            {
+                //call the Next() function of the random class with the value 0 and 2
+                int empAtt = random.Next(0, 3);
+
+                //switch statement
+                switch (empAtt)
+                {
+                    //when the employee is present for full time 
+                    case 1:
+                        //set the hour to 8
+                        hour += 8;
+                        //increase the days by 1
+                        days++;
+                        //Display employee is present for full time
+                        Console.WriteLine("Employee is present for Full time. Hours = {0}, Days = {1}", hour, days);
+
+                        //break the case 1
+                        break;
+                    //when the employee is present for part time
+                    case 2:
+                        //set the hour to 4
+                        hour += 4;
+                        //increase the days by 1
+                        days++;
+                        //Display employee present for part time
+                        Console.WriteLine("Employee is present for part time. Hours = {0}, Days = {1}", hour, days);
+
+                        //break the case 2
+                        break;
+
+                    default:
+                        //increase the days by 1
+                        days++;
+                        //Display employee is part time employee
+                        Console.WriteLine("Employee is Absent. Hours = {0}, Days = {1}", hour, days);
+                        
+                        //break the default case 
+                        break;
+                }
+            }
+            
+            //calculate the monthly wage by multiplaying the hour to employee hour rate and month days
+            MonthlyWage = hour * EMP_HOUR_RATE * EMP_MONTH_DAYS;
+
+            //Display the employee monthly wage
+            Console.WriteLine("Employee Monthly Wage is: " + MonthlyWage);
+        }
+    }
+}
